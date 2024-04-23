e9246664a419f0966a1ecd07307e5b12a3aa17c4
Felipe Quirino <felipe.quirino@ilegra.com>
2024-04-23 16:22:40 -0300

    v1

---
+++ src/main/java/com/felipe/quirino/temafinal1/Controller/temafinal1Controller.java
@@ -0,0 +1,17 @@
+package com.felipe.quirino.temafinal1.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class temafinal1Controller {
+
+    @GetMapping("/")
+    public String obterMensagem() {
+        return " \n" +
+                " +-+-+-+ +-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+-+-+\n" +
+                " |APP| |TEMA| |FINAL 1 FUNCIONANDO|\n" +
+                " +-+-+-+ +-+-+-+-+ +-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+-+-+\n ";
+    }
+}
---
+++ src/test/java/com/felipe/quirino/temafinal1/Temafinal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.quirino.temafinal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temafinal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
